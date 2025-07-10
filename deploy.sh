11e225dc8962bd20ea516b73ad08909324217fc5
Gustavo <mr.caraiba@gmail.com>
2025-07-10 00:55:43 -0300

    Deploy para GitHub Pages via docs

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ rm -rf build
 echo "📤 Commitando alterações..."
 git add .
 git commit -m "Deploy para GitHub Pages via docs"
-git push origin gh-pages
+git push --force origin gh-pages
 
 echo "🚀 Deploy atualizado na pasta /docs! Só ir nas configs do GitHub Pages e garantir que está usando ela 🎯"
